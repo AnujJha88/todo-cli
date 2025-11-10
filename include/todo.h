9d3d1c107e0bd25973b01387be362fac59fe2b97
AnujJha88 <anujjha8788@gmail.com>
2025-11-10 15:47:11 +0530

    todo planning

--- include/todo.h
+++ include/todo.h
@@ -8,9 +8,42 @@
 enum class Priority {LOW,MEDIUM,HIGH,URGENT};
 enum class Status {TODO,DOING,DONE,ARCHIVED};
 
-struct ToDo{
+struct Todo{
+int id;
+    std::string task;
+    std::string desc;
+    Priority priority;
+    Status status;
+    std::vector<std::string> tags;
+    time_t created_at;
+    time_t due;
+    time_t completed_at;
+
+    Todo();
+    std::string priority_str() const;
+    std::string status_str() const;
+    bool is_overdue() const;
 
 };
 
+
+class TodoList{
+private:
+    std::vector<Todo> todos;
+    int next_id;
+public:
+    TodoList();
+    void add();
+    void remove();
+    void update();
+    void set_status();
+    void set_priority();
+    void add_tags();
+    void set_due_date();
+
+    std::vector<Todo> get_all() const;
+    std::vector<Todo> filter_by_status(Status status) const;
+
+};
 
 #endif
