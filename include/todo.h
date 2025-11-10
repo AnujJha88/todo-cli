4146dd9164fffb886af4d388f6a928249c11e967
AnujJha88 <anujjha8788@gmail.com>
2025-11-10 11:35:50 +0530

    Initial commit

---
+++ include/todo.h
@@ -0,0 +1,16 @@
+#ifndef TODO_H
+#define TODO_H
+
+#include<string>
+#include<vector>
+#include<ctime>
+
+enum class Priority {LOW,MEDIUM,HIGH,URGENT};
+enum class Status {TODO,DOING,DONE,ARCHIVED};
+
+struct ToDo{
+
+};
+
+
+#endif
