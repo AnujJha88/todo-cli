dbe7f8c32c743904c149547c757de8bfead52058
AnujJha88 <anujjha8788@gmail.com>
2025-11-10 18:01:59 +0530

    todo list function signatures

--- include/todo.h
+++ include/todo.h
@@ -32,15 +32,17 @@ private:
     std::vector<Todo> todos;
     int next_id;
 public:
+    //constructor
     TodoList();
-    void add();
-    void remove();
-    void update();
-    void set_status();
-    void set_priority();
-    void add_tags();
-    void set_due_date();
-
+    //creat+update+delete functions
+    void add(const std::string &name, const std::string &desc="",Priority priority=Priority::MEDIUM,Status status=Status::TODO, const std::vector<std::string> tags={});
+    void remove(int id);
+    void update(int id, const std::string &name, const std::string &desc="", const std::vector<std::string> tags={});
+    void set_status(int id,Status status);
+    void set_priority(int id,Priority priority);
+    void add_tags(int id,const std::vector<std::string> &tags);
+    void set_due_date(int id,time_t due_date);
+    //fetchers
     std::vector<Todo> get_all() const;
     std::vector<Todo> filter_by_status(Status status) const;
     std::vector<Todo> filter_by_priority(Priority priority) const;
@@ -48,13 +50,15 @@ public:
     std::vector<Todo> filter_by_date(time_t date) const;
     std::vector<Todo> search(const std::string &query) const;
     std::vector<Todo> get_overdue() const;
-
     Todo* find_by_id( int id);
+
+    //sort
     void sort_priority();
     void sort_by_created();
     void sort_by_due();
 
-    size_t size() const;
+    //QoL utils
+    size_t size() const{return todos.size();}
     int get_next_id(){return next_id;}
 };
 
